03a1d7b0526ed0ce07909745ad85e03d661c91e3
Seyed Mohammad Ayyoubzadeh <s.m.ayyoubzadeh@gmail.com>
2024-03-10 13:33:28 +0330

    Add dynamic form

---
+++ SAMSITCMiniLIS/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace SAMSITCMiniLIS
+{
+    public class FileHelper
+    {
+        public static string REPORT_TEMPLATE_PATH = "report_templates";
+
+        public static string[] GetReportNames()
+        {
+            string[] filePaths= Directory.GetFiles(REPORT_TEMPLATE_PATH);
+            List<string> fileNames = new List<string>();
+            foreach (string file in filePaths)
+            {
+                fileNames.Add(Path.GetFileNameWithoutExtension(file));
+            }
+            return fileNames.ToArray();
+        }
+    }
+}
